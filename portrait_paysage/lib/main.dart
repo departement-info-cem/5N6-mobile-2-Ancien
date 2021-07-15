d88751776071b37648e28de9da25a185a1187aae
deguet <joris.deguet@gmail.com>
2021-07-15 07:59:44 -0400

    ajout d'un bool pour reagir au clic

--- portrait_paysage/lib/main.dart
+++ portrait_paysage/lib/main.dart
@@ -34,14 +34,14 @@ class _PortaitPaysageState extends State<PortaitPaysage> {
           if (orientation == Orientation.landscape) {
             return _buildWideContainers();
           } else {
-            return _buildNormalContainer();
+            return _buildNormalContainer(false);
           }
         },
       ),
     );
   }
 
-  Widget _buildNormalContainer() {
+  Widget _buildNormalContainer(bool avecDeuxPanneaux) {
     return ListView(
       children: liste.map(
         (e) => ListTile(
@@ -50,8 +50,13 @@ class _PortaitPaysageState extends State<PortaitPaysage> {
           title: Text(e),
           subtitle: Text('super description'),
           onTap: () {
-            this._selected = e;                     // la reaction a la selection va se faire au setState
-            setState(() {});
+            if (avecDeuxPanneaux) {
+              this._selected = e;                     // la reaction a la selection va se faire au setState
+              setState(() {});
+            } else {
+              // TODO ecrire le code pour passer a un autre ecran de detail de l'objet selectionne
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Coucou')));
+            }
           },
         )
       ).toList()
@@ -63,7 +68,7 @@ class _PortaitPaysageState extends State<PortaitPaysage> {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
-          Expanded(child: _buildNormalContainer()),        // reutilise la liste pour la partie gauche
+          Expanded(child: _buildNormalContainer(true)),        // reutilise la liste pour la partie gauche
           Expanded(
             flex: 2,
             child: this._selected == null ?                 // si on n'a rien de selectionne
