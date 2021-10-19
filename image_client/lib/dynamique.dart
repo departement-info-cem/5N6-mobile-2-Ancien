344e4897503f19b4ede28af91d7bc68305fe3987
Jean-Michel Nadeau <jean-michelnadeau@MacBook-Air-de-Jean-Michel.local>
2021-10-19 02:41:05 -0400

    Ajout de la séance 8.2

---
+++ image_client/lib/dynamique.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/fixe.dart';
+
+class DynamiquePage extends StatefulWidget {
+  const DynamiquePage({Key? key}) : super(key: key);
+
+  @override
+  _DynamiquePageState createState() => _DynamiquePageState();
+}
+
+class _DynamiquePageState extends State<DynamiquePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gestion de la taille des images'),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => FixePage(),
+                  ),
+                );
+              },
+              child: Text("Page avec tailles fixes")
+          ),
+
+          Expanded(
+            child: ListView(
+                children: [
+
+                  Row(
+                    children: [
+                      Expanded(
+                          flex:2,
+                          child:
+                              //TODO au lieu de mettre directement le widget Image.network, on l'encapsule (on le wrap)
+                              // dans un LayoutBuilder. Le LayoutBuilder nous permet d'avoir un nouveau build context
+                              // uniquement pour le widget.
+                          LayoutBuilder(
+                            builder: (BuildContext context, BoxConstraints constraints) {
+
+                              //TODO La MediaQuery permet de connaitre la taille disponible dans le build context,
+                              // ici build context est uniquement pour le widget Image.network, c'est donc la taille disponible
+                              // pour l'image
+                              var size = MediaQuery.of(context).size;
+
+                              //TODO la taille est en double, il sera important de convertir la taille en int
+                              // pour que le serveur prenne notre requête (ex: 390 au lieu de 390.0)
+                              String width = size.width.toInt().toString();
+
+                              //TODO Une fois la taille connue, il suffit de la spécifier dans l'URL
+                              return Image.network("https://exercices-web.herokuapp.com/exos/image?&width="+width, width: size.width,);
+                            }
+                          ),
+
+                      ),
+                      Spacer(),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child:
+                        LayoutBuilder(
+                            builder: (BuildContext context, BoxConstraints constraints) {
+                              var size = MediaQuery.of(context).size;
+
+                              String width = size.width.toInt().toString();
+
+                              return Image.network("https://exercices-web.herokuapp.com/exos/image?&width="+width, width: size.width,);
+                            }
+                        ),
+                      ),
+                      Spacer(),
+                    ],
+                  )
+                ]),
+          ),
+        ],
+      ),
+
+    );
+  }
+}
---
+++ image_client/lib/fixe.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/dynamique.dart';
+
+class FixePage extends StatefulWidget {
+  const FixePage({Key? key}) : super(key: key);
+
+  @override
+  _FixePageState createState() => _FixePageState();
+}
+
+class _FixePageState extends State<FixePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Gestion de la taille des images'),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DynamiquePage(),
+                  ),
+                );
+              },
+              child: Text("Page avec tailles dynamiques")
+          ),
+
+          Expanded(
+            child: ListView(
+              children: [
+
+                //TODO Pour utiliser des images de taille fixe, c'est plutôt simple,
+                // il suffit de spécifier au serveur la taille d'image désirée
+                // Soit la taille du contrôle
+                Image.network("https://exercices-web.herokuapp.com/exos/image?width=100", width: 100,),
+                Image.network("https://exercices-web.herokuapp.com/exos/image?width=150", width: 150,),
+                Image.network("https://exercices-web.herokuapp.com/exos/image?width=200", width: 200,),
+                Image.network("https://exercices-web.herokuapp.com/exos/image?width=300", width: 300,),
+                Image.network("https://exercices-web.herokuapp.com/exos/image?width=400", width: 400,),
+              ]),
+          ),
+        ],
+      ),
+
+    );
+
+
+  }
+}
